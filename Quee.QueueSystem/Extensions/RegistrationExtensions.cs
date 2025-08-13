7b4472a4b7e891c1def39f64253a369671b4f896
DavidBaillie <DavidlBaillie@gmail.com>
2025-08-13 19:35:25 -0400

    Cleanup Registration
    - Add extension to ServiceCollection
    - Remove registration from webapp

--- Quee.QueueSystem/Extensions/RegistrationExtensions.cs
+++ Quee.QueueSystem/Extensions/RegistrationExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Quee.AzureServiceBus;
 using Quee.Interfaces;
 using Quee.Memory;
+using Quee.Services;
 
 namespace Quee.Extensions;
 
@@ -16,7 +18,7 @@ public static class RegistrationExtensions
     /// <param name="services">Service collection for depenency injection</param>
     /// <param name="connectionString">Connection string to the service bus</param>
     /// <param name="configuration">Options object for configuration</param>
-    /// <returns>Cascading reference</returns>
+    /// <returns>Cascading rerence to <see cref="IServiceCollection"/></returns>
     public static IServiceCollection QueeWithAzureServiceBus(
         this IServiceCollection services,
         string connectionString,
@@ -33,13 +35,34 @@ public static class RegistrationExtensions
     /// </summary>
     /// <param name="services">Service collection for dependency injection</param>
     /// <param name="configuration">Options object for configuration</param>
-    /// <returns>Cascading reference</returns>
+    /// <returns>Cascading rerence to <see cref="IServiceCollection"/></returns>
     public static IServiceCollection QueeInMemory(
         this IServiceCollection services,
         Action<IInMemoryQueueConfigurator>? configuration = null)
     {
         IInMemoryQueueConfigurator configurator = new InMemoryQueueConfigurator(services);
         configuration?.Invoke(configurator);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the message tracking service into the send/receive system to allow you to monitor all messages sent and received by the local runtime.
+    /// <para>CAUTION: This will impact performance and should only be added in development or test environments where tracking all messages is important.</para>
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> to register monitor with</param>
+    /// <param name="maximumMessagesPerQueue">The maxmimum number of messages that will be stored for each queue in local memory</param>
+    /// <returns>Cascading rerence to <see cref="IServiceCollection"/></returns>
+    public static IServiceCollection AddQueeMessageTracker(this IServiceCollection services, int maximumMessagesPerQueue = 100000)
+    {
+        services.RemoveAll<IQueueMonitor>();
+        services.RemoveAll<IQueueEventTrackingService>();
+
+        services.AddTransient<IQueueMonitor, QueueMonitor>();
+        services.AddSingleton<IQueueEventTrackingService>((provider) =>
+        {
+            return new QueueEventTrackingService(maximumMessagesPerQueue);
+        });
 
         return services;
     }
--- Quee.WebApp/Program.cs
+++ Quee.WebApp/Program.cs
@@ -1,7 +1,3 @@
-using Quee.Extensions;
-using Quee.WebApp.Quee.Commands;
-using Quee.WebApp.Quee.Consumers;
-
 namespace QueueUtility.WebApp
 {
     public class Program
@@ -9,39 +5,11 @@ namespace QueueUtility.WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            //builder.Services.QueeWithAzureServiceBus(builder.Configuration["ServiceBusConnectionString"]!, options =>
-            //{
-            //    options.DisableRetryPolicy();
-            //    options.AddQueueMessageTracker()
-            //        .AddQueueProcessors<LogMessageCommand, LogMessageConsumer>("LogMessage-Queue")
-            //        .AddQueueProcessors<FailMessageCommand, FailMessageConsumer>("FailMessage-Queue",
-            //            TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10));
-            //});
-
-            builder.Services.QueeInMemory(options =>
-            {
-                //options.DisableRetryPolicy();
-                options.AddQueueMessageTracker()
-                    .AddQueueProcessors<LogMessageCommand, LogMessageConsumer>("LogMessage-Queue")
-                    .AddQueueProcessors<FailMessageCommand, FailMessageConsumer>("FailMessage-Queue",
-                        TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-            });
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
