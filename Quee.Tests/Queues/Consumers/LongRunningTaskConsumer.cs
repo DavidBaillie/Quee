18da2ad4143771de8f716b89485279b4b9ac25d9
DavidBaillie <DavidlBaillie@gmail.com>
2025-08-13 20:28:42 -0400

    Adding test project and a single test to prove it works

---
+++ Quee.Tests/Queues/Consumers/LongRunningTaskConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Quee.Interfaces;
+using Quee.Tests.Queues.Commands;
+
+namespace Quee.Tests.Queues.Consumers;
+
+/// <summary>
+/// Generic consumer that delays completion for a set number of milliseconds to simulate some asyncronous activity
+/// </summary>
+internal class LongRunningTaskConsumer : IConsumer<LongRunningTaskCommand>
+{
+    /// <summary>
+    /// Consumes the delay message, causing a delay of <see cref="LongRunningTaskCommand.milisecondsToWait"/> milliseconds
+    /// </summary>
+    /// <param name="message">Message with delay time</param>
+    /// <param name="cancellationToken">Process token</param>
+    public async Task ConsumeAsync(LongRunningTaskCommand message, CancellationToken cancellationToken)
+    {
+        await Task.Delay(message.milisecondsToWait);
+    }
+
+    /// <summary>
+    /// Consumes the fault of the message not delaying correctly? Should never really happen. 
+    /// </summary>
+    /// <param name="fault">Faulted message</param>
+    /// <param name="cancellationToken">Process token</param>
+    public Task ConsumeFaultAsync(IFault<LongRunningTaskCommand> fault, CancellationToken cancellationToken)
+    {
+        // Should never run
+        return Task.CompletedTask;
+    }
+}
